a1f3834a687767510a0753fb938c21a1b98c88bd
Anjali <agali1@iastate.edu>
2025-12-02 20:38:01 -0600

    Added sections to add each individual person to the It's a Conspiracy Dev Website
    Hey Audrey I left a comment letting u know that we need to add: pronouns, Fav Video Game, Portfolio, and Social Media

--- src/data/dev.js
+++ src/data/dev.js
@@ -1,5 +1,5 @@
 import link from "../assets/link.png"
-
+// NOTE TO AUDREY: Yo wassup we need to add a section for pronouns, Fav Video Game, Portfolio, and Social Media
 const DevTeam = [
     {
         id:0,
@@ -30,6 +30,327 @@ const DevTeam = [
         skills: "Creativity",
         email: "",
         fact: "He's obsessed with H.G. Wells' The Time Machine and is writing his own version."
+    },
+    {
+        id:3,
+        img: link,
+        name: "Grace Welch",
+        title: "Game/Level Designer & Prototype Designer",
+        role: "Production, Development",
+        skills: "The Ocarina of Time & the Chillrend Sword",
+        email: "gracewelch@boundlessgamers.org",
+        fact: "Lost only one point on the Full Sail University Global Professionalism Score because I slept in on a Zoom meeting."
+    },
+
+    {
+        id:4,
+        img: link,
+        name: "Anjali Gali",
+        title: "Character & Environment Artist, Web Designer, Programmer, UI/UX Developer",
+        role: "Art, Web Design",
+        skills: "Healing Potion, Food",
+        email: "anjaligali@boundlessgamers.org",
+        fact: "I used to be a teacher!"
+    },
+    {
+        id:5,
+        img: link,
+        name: "Emma Puissegur",
+        title: "Audio Engineer, Composer",
+        role: "Audio",
+        skills: "Crowbar and a Magical Gemstone",
+        email: "emmapuissegur@boundlessgamers.org",
+        fact: "I've been a flute player and a singer for almost 15 years"
+    },
+    {
+        id:6,
+        img: link,
+        name: "Zack Eddy",
+        title: "Animator/3D Artist ",
+        role: "Production Support, Development",
+        skills: "Crowbar and a Magical Gemstone",
+        email: "zack.eddy@boundlessgamers.org",
+        fact: "I'm a black belt in Tae Kwon Do, and world class marching Cymbalist"
+    },
+    {
+        id:7,
+        img: link,
+        name: "Sapphira Pack",
+        title: "Team Assistant",
+        role: "Art",
+        skills: "Flashlight and some teriyaki jerky",
+        email: "sapphirap@boundlessgamers.org",
+        fact: "I do some streaming on Twitch as a VTuber."
+    },
+    {
+        id:8,
+        img: link,
+        name: "Rachael Utting",
+        title: "Concept Artist. Character Artist",
+        role: "Art",
+        skills: "Bag of Holding and Murray the Skull",
+        email: "rachaelautting@boundlessgamers.org",
+        fact: "Arrived at Rachael's second cousin's 21st formal dress party with her Cousin both dressed as pirates, when asked 'why are you dressed as pirates' Their response was 'Because we Arrrrrr!'"
+    },
+    {
+        id:9,
+        img: link,
+        name: "Rachael Utting",
+        title: "Concept Artist. Character Artist",
+        role: "Art",
+        skills: "Flashlight and some teriyaki jerky",
+        email: "rachaelautting@boundlessgamers.org",
+        fact: "Arrived at Rachael's second cousin's 21st formal dress party with her Cousin both dressed as pirates, when asked 'why are you dressed as pirates' Their response was 'Because we Arrrrrr!'"
+    },
+    {
+        id:10,
+        img: link,
+        name: "Rachael Utting",
+        title: "Concept Artist. Character Artist",
+        role: "Art",
+        skills: "Flashlight and some teriyaki jerky",
+        email: "rachaelautting@boundlessgamers.org",
+        fact: "Arrived at Rachael's second cousin's 21st formal dress party with her Cousin both dressed as pirates, when asked 'why are you dressed as pirates' Their response was 'Because we Arrrrrr!'"
+    },
+    {
+        id:11,
+        img: link,
+        name: "Rachael Utting",
+        title: "Concept Artist. Character Artist",
+        role: "Art",
+        skills: "Flashlight and some teriyaki jerky",
+        email: "rachaelautting@boundlessgamers.org",
+        fact: "Arrived at Rachael's second cousin's 21st formal dress party with her Cousin both dressed as pirates, when asked 'why are you dressed as pirates' Their response was 'Because we Arrrrrr!'"
+    },
+    {
+        id:12,
+        img: link,
+        name: "Rachael Utting",
+        title: "Concept Artist. Character Artist",
+        role: "Art",
+        skills: "Flashlight and some teriyaki jerky",
+        email: "rachaelautting@boundlessgamers.org",
+        fact: "Arrived at Rachael's second cousin's 21st formal dress party with her Cousin both dressed as pirates, when asked 'why are you dressed as pirates' Their response was 'Because we Arrrrrr!'"
+    },
+    {
+        id:13,
+        img: link,
+        name: "Rachael Utting",
+        title: "Concept Artist. Character Artist",
+        role: "Art",
+        skills: "Flashlight and some teriyaki jerky",
+        email: "rachaelautting@boundlessgamers.org",
+        fact: "Arrived at Rachael's second cousin's 21st formal dress party with her Cousin both dressed as pirates, when asked 'why are you dressed as pirates' Their response was 'Because we Arrrrrr!'"
+    },
+    {
+        id:14,
+        img: link,
+        name: "Rachael Utting",
+        title: "Concept Artist. Character Artist",
+        role: "Art",
+        skills: "Flashlight and some teriyaki jerky",
+        email: "rachaelautting@boundlessgamers.org",
+        fact: "Arrived at Rachael's second cousin's 21st formal dress party with her Cousin both dressed as pirates, when asked 'why are you dressed as pirates' Their response was 'Because we Arrrrrr!'"
+    },
+    {
+        id:15,
+        img: link,
+        name: "Rachael Utting",
+        title: "Concept Artist. Character Artist",
+        role: "Art",
+        skills: "Flashlight and some teriyaki jerky",
+        email: "rachaelautting@boundlessgamers.org",
+        fact: "Arrived at Rachael's second cousin's 21st formal dress party with her Cousin both dressed as pirates, when asked 'why are you dressed as pirates' Their response was 'Because we Arrrrrr!'"
+    },
+    {
+        id:16,
+        img: link,
+        name: "Rachael Utting",
+        title: "Concept Artist. Character Artist",
+        role: "Art",
+        skills: "Flashlight and some teriyaki jerky",
+        email: "rachaelautting@boundlessgamers.org",
+        fact: "Arrived at Rachael's second cousin's 21st formal dress party with her Cousin both dressed as pirates, when asked 'why are you dressed as pirates' Their response was 'Because we Arrrrrr!'"
+    },
+    {
+        id:17,
+        img: link,
+        name: "Rachael Utting",
+        title: "Concept Artist. Character Artist",
+        role: "Art",
+        skills: "Flashlight and some teriyaki jerky",
+        email: "rachaelautting@boundlessgamers.org",
+        fact: "Arrived at Rachael's second cousin's 21st formal dress party with her Cousin both dressed as pirates, when asked 'why are you dressed as pirates' Their response was 'Because we Arrrrrr!'"
+    },
+    {
+        id:18,
+        img: link,
+        name: "Rachael Utting",
+        title: "Concept Artist. Character Artist",
+        role: "Art",
+        skills: "Flashlight and some teriyaki jerky",
+        email: "rachaelautting@boundlessgamers.org",
+        fact: "Arrived at Rachael's second cousin's 21st formal dress party with her Cousin both dressed as pirates, when asked 'why are you dressed as pirates' Their response was 'Because we Arrrrrr!'"
+    },
+    {
+        id:19,
+        img: link,
+        name: "Rachael Utting",
+        title: "Concept Artist. Character Artist",
+        role: "Art",
+        skills: "Flashlight and some teriyaki jerky",
+        email: "rachaelautting@boundlessgamers.org",
+        fact: "Arrived at Rachael's second cousin's 21st formal dress party with her Cousin both dressed as pirates, when asked 'why are you dressed as pirates' Their response was 'Because we Arrrrrr!'"
+    },
+    {
+        id:20,
+        img: link,
+        name: "Rachael Utting",
+        title: "Concept Artist. Character Artist",
+        role: "Art",
+        skills: "Flashlight and some teriyaki jerky",
+        email: "rachaelautting@boundlessgamers.org",
+        fact: "Arrived at Rachael's second cousin's 21st formal dress party with her Cousin both dressed as pirates, when asked 'why are you dressed as pirates' Their response was 'Because we Arrrrrr!'"
+    },
+    {
+        id:21,
+        img: link,
+        name: "Rachael Utting",
+        title: "Concept Artist. Character Artist",
+        role: "Art",
+        skills: "Flashlight and some teriyaki jerky",
+        email: "rachaelautting@boundlessgamers.org",
+        fact: "Arrived at Rachael's second cousin's 21st formal dress party with her Cousin both dressed as pirates, when asked 'why are you dressed as pirates' Their response was 'Because we Arrrrrr!'"
+    },
+    {
+        id:22,
+        img: link,
+        name: "Rachael Utting",
+        title: "Concept Artist. Character Artist",
+        role: "Art",
+        skills: "Flashlight and some teriyaki jerky",
+        email: "rachaelautting@boundlessgamers.org",
+        fact: "Arrived at Rachael's second cousin's 21st formal dress party with her Cousin both dressed as pirates, when asked 'why are you dressed as pirates' Their response was 'Because we Arrrrrr!'"
+    },
+    {
+        id:23,
+        img: link,
+        name: "Rachael Utting",
+        title: "Concept Artist. Character Artist",
+        role: "Art",
+        skills: "Flashlight and some teriyaki jerky",
+        email: "rachaelautting@boundlessgamers.org",
+        fact: "Arrived at Rachael's second cousin's 21st formal dress party with her Cousin both dressed as pirates, when asked 'why are you dressed as pirates' Their response was 'Because we Arrrrrr!'"
+    },
+    {
+        id:24,
+        img: link,
+        name: "Rachael Utting",
+        title: "Concept Artist. Character Artist",
+        role: "Art",
+        skills: "Flashlight and some teriyaki jerky",
+        email: "rachaelautting@boundlessgamers.org",
+        fact: "Arrived at Rachael's second cousin's 21st formal dress party with her Cousin both dressed as pirates, when asked 'why are you dressed as pirates' Their response was 'Because we Arrrrrr!'"
+    },
+    {
+        id:25,
+        img: link,
+        name: "Rachael Utting",
+        title: "Concept Artist. Character Artist",
+        role: "Art",
+        skills: "Flashlight and some teriyaki jerky",
+        email: "rachaelautting@boundlessgamers.org",
+        fact: "Arrived at Rachael's second cousin's 21st formal dress party with her Cousin both dressed as pirates, when asked 'why are you dressed as pirates' Their response was 'Because we Arrrrrr!'"
+    },
+    {
+        id:26,
+        img: link,
+        name: "Rachael Utting",
+        title: "Concept Artist. Character Artist",
+        role: "Art",
+        skills: "Flashlight and some teriyaki jerky",
+        email: "rachaelautting@boundlessgamers.org",
+        fact: "Arrived at Rachael's second cousin's 21st formal dress party with her Cousin both dressed as pirates, when asked 'why are you dressed as pirates' Their response was 'Because we Arrrrrr!'"
+    },
+    {
+        id:27,
+        img: link,
+        name: "Rachael Utting",
+        title: "Concept Artist. Character Artist",
+        role: "Art",
+        skills: "Flashlight and some teriyaki jerky",
+        email: "rachaelautting@boundlessgamers.org",
+        fact: "Arrived at Rachael's second cousin's 21st formal dress party with her Cousin both dressed as pirates, when asked 'why are you dressed as pirates' Their response was 'Because we Arrrrrr!'"
+    },
+    {
+        id:28,
+        img: link,
+        name: "Rachael Utting",
+        title: "Concept Artist. Character Artist",
+        role: "Art",
+        skills: "Flashlight and some teriyaki jerky",
+        email: "rachaelautting@boundlessgamers.org",
+        fact: "Arrived at Rachael's second cousin's 21st formal dress party with her Cousin both dressed as pirates, when asked 'why are you dressed as pirates' Their response was 'Because we Arrrrrr!'"
+    },
+    {
+        id:29,
+        img: link,
+        name: "Rachael Utting",
+        title: "Concept Artist. Character Artist",
+        role: "Art",
+        skills: "Flashlight and some teriyaki jerky",
+        email: "rachaelautting@boundlessgamers.org",
+        fact: "Arrived at Rachael's second cousin's 21st formal dress party with her Cousin both dressed as pirates, when asked 'why are you dressed as pirates' Their response was 'Because we Arrrrrr!'"
+    },
+    {
+        id:30,
+        img: link,
+        name: "Rachael Utting",
+        title: "Concept Artist. Character Artist",
+        role: "Art",
+        skills: "Flashlight and some teriyaki jerky",
+        email: "rachaelautting@boundlessgamers.org",
+        fact: "Arrived at Rachael's second cousin's 21st formal dress party with her Cousin both dressed as pirates, when asked 'why are you dressed as pirates' Their response was 'Because we Arrrrrr!'"
+    },
+    {
+        id:31,
+        img: link,
+        name: "Rachael Utting",
+        title: "Concept Artist. Character Artist",
+        role: "Art",
+        skills: "Flashlight and some teriyaki jerky",
+        email: "rachaelautting@boundlessgamers.org",
+        fact: "Arrived at Rachael's second cousin's 21st formal dress party with her Cousin both dressed as pirates, when asked 'why are you dressed as pirates' Their response was 'Because we Arrrrrr!'"
+    },
+    {
+        id:32,
+        img: link,
+        name: "Rachael Utting",
+        title: "Concept Artist. Character Artist",
+        role: "Art",
+        skills: "Flashlight and some teriyaki jerky",
+        email: "rachaelautting@boundlessgamers.org",
+        fact: "Arrived at Rachael's second cousin's 21st formal dress party with her Cousin both dressed as pirates, when asked 'why are you dressed as pirates' Their response was 'Because we Arrrrrr!'"
+    },
+    {
+        id:33,
+        img: link,
+        name: "Rachael Utting",
+        title: "Concept Artist. Character Artist",
+        role: "Art",
+        skills: "Flashlight and some teriyaki jerky",
+        email: "rachaelautting@boundlessgamers.org",
+        fact: "Arrived at Rachael's second cousin's 21st formal dress party with her Cousin both dressed as pirates, when asked 'why are you dressed as pirates' Their response was 'Because we Arrrrrr!'"
+    },
+    {
+        id:34   ,
+        img: link,
+        name: "Rachael Utting",
+        title: "Concept Artist. Character Artist",
+        role: "Art",
+        skills: "Flashlight and some teriyaki jerky",
+        email: "rachaelautting@boundlessgamers.org",
+        fact: "Arrived at Rachael's second cousin's 21st formal dress party with her Cousin both dressed as pirates, when asked 'why are you dressed as pirates' Their response was 'Because we Arrrrrr!'"
     },
 ];
 
